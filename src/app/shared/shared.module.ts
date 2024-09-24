b9fe8540689866ecc860b2e27e80af3c708dac0f
AngelCortesM <joseangelcortesmontoya@gmail.com>
2024-09-24 18:39:23 -0500

    clase 108

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LazyImageComponent
+  ],
   imports: [
     CommonModule,
     SidebarComponent
   ],
-  exports: [SidebarComponent]
+  exports: [SidebarComponent,LazyImageComponent]
 })
 export class SharedModule { }
